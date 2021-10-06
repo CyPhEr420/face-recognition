90ce2658ff176dda3c7ff40a5d11afd8d9b544d9
CyPhEr420 <vimalvk9900@gmail.com>
2021-10-06 13:37:33 +0530

    mobile style v1

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import brain from './brain.png';
 
 const Logo = () => {
     return (
-        <div className='  ma4 mt0'>
+        <div className='  ma4 logo-center mt0'>
             <Tilty className=" Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
                 <div className="pa3 Tilt-inner"> <img style={{ paddingTop: '5px' }} src={brain} alt='logo' height="100px"></img> </div>
             </Tilty>

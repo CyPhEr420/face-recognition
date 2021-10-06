d200b80a85dce0f3ea2a932aa4eb523b9424dbc4
CyPhEr420 <vimalvk9900@gmail.com>
2021-10-06 12:29:56 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,217 @@
+import React from 'react';
+import { Component } from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register'
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+      "value": 150,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 10,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 20,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  }
+}
+
+
+
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  }
+
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      }
+    })
+  }
+
+
+  calculateFaceLocation = (data) => {
+    const box = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      left: box.left_col * width,
+      top: box.top_row * height,
+      right: width - (box.right_col * width),
+      bottom: height - (box.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+  onPictureSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    console.log('url', this.state.imageUrl)
+    console.log('input', this.state.input)
+    fetch('https://still-brook-25458.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input,
+      })
+    }).then(response => response.json())
+      .then((response) => {
+        if (response) {
+          fetch('https://still-brook-25458.herokuapp.com/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            })
+          }).then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(err => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initialState)
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route })
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+
+        {route === 'home' ?
+          <div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          : (
+            route === 'signin'
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> :
+              (
+                route === 'signout'
+                  ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                  : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+              )
+
+          )
+        }
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Tilty from 'react-tilty';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+    return (
+        <div className='  ma4 mt0'>
+            <Tilty className=" Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="pa3 Tilt-inner"> <img style={{ paddingTop: '5px' }} src={brain} alt='logo' height="100px"></img> </div>
+            </Tilty>
+
+        </div>
+    );
+}
+
+export default Logo;
